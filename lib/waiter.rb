4972e476a2d6ceda1113d38f1f445ac6785b0334
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-06-13 09:35:11 -0500

    has many through lab

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
 class Waiter
 
-end
+    attr_accessor :name, :yrs_experience
+  
+    @@all = []
+  
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def new_meal(customer_instance, total, tip)
+        meal_instance = Meal.new(self,customer_instance, total, tip)
+    end
+
+    def meals
+        Meal.all.select{
+            |meal| meal.waiter == self
+        }
+    end
+    
+    def best_tipper
+        Meal.all.max_by{
+            |meal| meal.tip
+        }.customer
+    end
+  
+  end 
